844631c9752c5d7d7ad38515f60f86f4572a06c8
Tara Evans <tarajevans@hotmail.com>
2022-06-25 11:05:21 -0400

    created code to display questions and answers, worked on some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
 var counter = 75;
+var questionIndex=0;
+var questionTextArea = document.querySelector("#questions");
+var timerDisplay = document.querySelector("#timer");
+var answerDisplayArea = document.querySelector("#answers");
+var answerOL = document.querySelector("answerOL");
 
 function countDown(){
 
 var countdown = function () {
-    console.log(counter);
+    timerDisplay.textContent= "Time Left: " + counter +"s";
     counter--;
     if(counter === 0) {
         console.log("Time's Up!");
@@ -15,83 +20,90 @@ var startCountdown = setInterval(countdown, 1000);
 }
 // an array to store the questions
 // Reference to Questions and Answers taken from Michael Rudolph/ReindeerCode https://github.com/ReindeerCode/Web-APIs-Code-Quiz/blob/master/JS/questions.js
+
 var questions = [
-    ["Which one is a looping structure in JavaScript?", 0],
-    ["What are the two basic groups of data types in JavaScript?", 1],
-    ["Commonly used data types DO NOT include:", 2],
-    ["Boolean operators that can be used in JavaScript include:", 3],
-    ["Which one of these is not among the three different types of errors in JavaScript?", 0],
-    ["What is the data type of variables in JavaScript?", 0],
-    ["The condition in an if / else statement is enclosed within ____.", 2],
-    ["Arrays in JavaScript can be used to store ____.", 3],
-    ["String values must be enclosed within ____ when being assigned to variables.", 2],
-    ["What is the type of Pop up boxes available in JavaScript?:", 3],
-];
-// an array to store the answer choices
-var answers = [
-    ["All the below", 
-    "For", 
-    "While", 
-    "do-while loops"], 
-    //Correct = "All the below"
-    ["Primitive and attribute", 
-    "Primitive and reference types", 
-    "Reference types and attribute", 
-    "None of the above"], 
-    //Correct = "Primitive and reference types"
-    ["strings", 
-    "booleans", 
-    "alerts", 
-    "numbers"],
-    //Correct ="alerts"
-    ["'And' Operator &&", 
-    "'Or' Operator ||", 
-    "'Not' Operator !", 
-    "All the above"], 
-    //Correct ="All the above"
-    ["Animation time errors", 
-    "Load time errors", 
-    "Run time errors", 
-    "Logical Errors"], 
-    //Correct="Animation time errors"
-    ["Object data types", 
-    "Function data type", 
-    "None of the above", 
-    "All of the above"],
-    //Correct="Object data types"
-    ["quotes", 
-    "curly brackets", 
-    "parentheses", 
-    "square brackets"],
-    //Correct="parentheses"
-    ["numbers and strings", 
-    "other arrays", 
-    "booleans", 
-    "all of the above"], 
-    //Correct="all of the above"
-    ["commas", 
-    "curly brackets", 
-    "quotes", 
-    "parentheses"],
-    //Correct="quotes"
-    ["Alert", 
-    "Confirm",
-    "Prompt", 
-    "All the above"]
-    //Correct="All the above"
-];
+    {
+        question: "Which one is a looping structure in JavaScript?",
+        answers: ["All the below", "For", "While", "do-while loops"],
+        correct: "All the below"
+    },
+    {
+        question: "What are the two basic groups of data types in JavaScript?", 
+        answers: ["Primitive and attribute", "Primitive and reference types", "Reference types and attribute", "None of the above"],
+        correct: "Primitive and reference types"
+    },
+    {
+        question: "Commonly used data types DO NOT include:", 
+        answers: ["strings", "booleans", "alerts", "numbers"],
+        correct: "alerts"
+    },
+    {
+        question: "Boolean operators that can be used in JavaScript include:", 
+        answers: ["'And' Operator &&", "'Or' Operator ||", "'Not' Operator !", "All the above"],
+        correct: "All the above"
+    },
+    {
+        question: "Which one of these is not among the three different types of errors in JavaScript?", 
+        answers: ["Animation time errors", "Load time errors", "Run time errors", "Logical Errors"],
+        correct: "Animation time errors"
+    },
+    {
+        question: "What is the data type of variables in JavaScript?", 
+        answers: ["Object data types", "Function data type", "None of the above", "All of the above"],
+        correct: "Object data types"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within __.", 
+        answers:  ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correct: "parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store __.", 
+        answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within __ when being assigned to variables.", 
+        answers: ["commas", "curly brackets", "quotes", "parentheses"],
+        correct: "quotes"
+    },
+    {
+        question: "What is the type of Pop up boxes available in JavaScript?:", 
+        answers: ["Alert",  "Confirm", "Prompt", "All the above"], 
+        correct: "All the above"
+    }];
+
+// main function to control test
+function startTest() {
+    counter = 75;
+    countdown();
+
+    displayQuestion(questionIndex);
+    displayAnswers(questionIndex);
+    advanceQuestionIndex();
+}
+function checkCorrectAnswer(qIndex, selectedAnswer) {
+    corAnswer = questions[qIndex].correct;
+if (selectedAnswer === corAnswer) {
+    console.log("Correct!")
+    } else {
+        console.log("Incorrect!");
+    }
+
+}
 // Create Buttons
 var testButton = document.querySelector("button[name='testButton']");
 var countDownButton = document.querySelector("button[name='startCountDown']");
 
+function displayQuestion(qIndex) {
+    questionTextArea.value = questions[qIndex][0];
+}
 // A function to test code with 
 function testFunct(){
     penalty();
 
 }
-for (var questionIndex=0; questionIndex < questions.length; questionIndex ++) {
-    console.log (questions [questionIndex] [0] + answers [questionIndex] + questions [questionIndex] [1]);
-}
+
 
 // penalty for wrong answer
 function penalty(){
